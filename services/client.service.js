2ceeeed52ef009b1192fdd8b6ee0bc15ee56fd15
Marco Juárez <marcojm28@hotmail.com>
2021-04-10 13:13:45 -0500

    Se añade método de kpi

--- services/client.service.js
+++ services/client.service.js
@@ -7,28 +7,43 @@ const service = {
         function (error, results, fields) {
           if (error) res.send(error);
 
-          results.forEach((result) => {
-            // console.log(result);
-          });
-
           res.status(200).send({ results });
         }
       );
     },
     kpiclients(req, res) {
-        conexion.query(
-          "SELECT * FROM clientes",
-          function (error, results, fields) {
-            if (error) res.send(error);
-  
-            results.forEach((result) => {
-              // console.log(result);
-            });
-  
-            res.status(200).send({ results });
+      var kpi = {
+        average: 0.0,
+        standardDeviation: 0.0,
+      };
+
+      let years = 0.0;
+      let standardDeviation = 0.0;
+      let variance = 0.0;
+
+      conexion.query(
+        "SELECT * FROM clientes",
+        function (error, results, fields) {
+          if (error) res.send(error);
+
+          results.forEach((client) => {
+            years = years + client.Edad;
+          });
+
+          kpi.average = years / results.length;
+
+          for (var i = 0; i < results.length; i++) {
+            var range;
+            range = Math.pow(results[i].Edad - kpi.average, 2);
+            variance = variance + range;
           }
-        );
-      },
+          
+          variance = variance / results.length;
+          kpi.standardDeviation = Math.sqrt(variance);
+          res.status(200).send({ kpi });
+        }
+      );
+    },
     createClient(req, res) {
       conexion.query(
         "INSERT INTO clientes SET ?",
